ee9c321f7e3a4ca261cf18f2cc2202d4ddcdf6ff
ゆなっぺ <j2206@matsue-ct.ac.jp>
2025-08-07 12:33:43 +0000

    カレンダー6-1

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); // 既存のDB接続モジュール
+const db = require('../db');
 
-// 認証チェック用ミドルウェア
-/*function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated && req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/signin'); // ログインしてない場合はサインインページへ
-}*/
-
-// カレンダーページ表示
-router.get('/', ensureAuthenticated, async (req, res) => {
+// 認証なしでカレンダー表示
+router.get('/', async (req, res) => {
   const [rows] = await db.query('SELECT * FROM events ORDER BY date');
   res.render('calendar', { events: rows });
 });
